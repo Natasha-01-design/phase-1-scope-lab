0fa9e4dbd0dd3a7e338238a55bdf822ff791c5c7
Natasha-01-design <natashakarwitha6@gmail.com>
2025-06-18 07:06:48 +0300

    me

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName='bob';
+var bestCustomer;
+function setBestCustomer(){
+ bestCustomer='not bob'
+}
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function overwriteBestCustomer() { 
+     bestCustomer='maybe bob'
+}
+
+    const leastFavoriteCusomer= 'Ali'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCusomer='someone else'
+    
+}
